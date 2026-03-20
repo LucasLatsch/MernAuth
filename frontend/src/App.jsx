cb28330304bfcb3188b9c4c6ea0775d802b8a79c
LucasLatsch <bello-lucas@hotmail.com>
2026-03-20 17:42:51 -0300

    feat: initialize frontend with React, Vite, Tailwind and Router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex items-center h-screen justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default App;
